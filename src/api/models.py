c140e3935b286e491d64910ab8396177598e550a
christianmartinroffey <c.martinroffey@gmail.com>
2022-07-05 18:47:24 +0000

    should have fixed the foreignkey error

--- src/api/models.py
+++ src/api/models.py
@@ -11,20 +11,20 @@ class User(db.Model):
 
 class Chore(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, ForeignKey(User.id), nullable=False)
+    user_id = db.Column(db.Integer, ForeignKey('User.id'), nullable=False)
     name = db.Column(db.String(120), unique=True, nullable=False)
     duration = db.Column(db.Integer, unique=False, nullable=False)
     date = db.Column(db.Datetime, unique=False, nullable=True)
 
 class Team(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column (db.Integer, ForeignKey(User.id), nullable=False)
+    user_id = db.Column (db.Integer, ForeignKey('User.id'), nullable=False)
     name = db.Column(db.String(120), unique=True, nullable=False)
 
 class Metrics(db.Model):
-    user_id = db.Column(db.Integer, ForeignKey(User.id), nullable=False)
-    chore_id = db.Column(db.Integer, ForeignKey(Chore.id), nullable=False)
-    chore_name = db.Column(db.String(120),ForeignKey(Chore.name), nullable=False)
+    user_id = db.Column(db.Integer, ForeignKey('User.id'), nullable=False)
+    chore_id = db.Column(db.Integer, ForeignKey('Chore.id'), nullable=False)
+    chore_name = db.Column(db.String(120),ForeignKey('Chore.name'), nullable=False)
     total_duration = db.Column(db.Integer, unique=False, nullable=False)
     time_range = db.Column(db.Datetime, unique=False, nullable=True)
 
