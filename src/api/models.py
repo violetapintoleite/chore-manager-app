bf239908c6d4a4e95f806a043097612fcef7a073
christianmartinroffey <c.martinroffey@gmail.com>
2022-07-05 17:53:34 +0000

    added the tables to models.py file

--- src/api/models.py
+++ src/api/models.py
@@ -5,9 +5,30 @@ db = SQLAlchemy()
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
+    username = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
 
+class Chore(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, ForeignKey(User.id), nullable=False)
+    name = db.Column(db.String(120), unique=True, nullable=False)
+    duration = db.Column(db.Integer, unique=False, nullable=False)
+    date = db.Column(db.Datetime, unique=False, nullable=True)
+
+class Team(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column (db.Integer, ForeignKey(User.id), nullable=False)
+    name = db.Column(db.String(120), unique=True, nullable=False)
+
+class Metrics(db.Model):
+    user_id = db.Column(db.Integer, ForeignKey(User.id), nullable=False)
+    chore_id = db.Column(db.Integer, ForeignKey(Chore.id), nullable=False)
+    chore_name = db.Column(db.String(120),ForeignKey(Chore.name), nullable=False)
+    total_duration = db.Column(db.Integer, unique=False, nullable=False)
+    time_range = db.Column(db.Datetime, unique=False, nullable=True)
+
+    
     def __repr__(self):
         return f'<User {self.email}>'
 
