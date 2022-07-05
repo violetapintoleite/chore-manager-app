cb9bdfc6f5f1b4d8f5a577b29594d8ce0a44f127
Violeta Pinto Leite <violetalopesalves@gmail.com>
2022-07-05 19:19:15 +0000

    Fixed model error

--- src/api/models.py
+++ src/api/models.py
@@ -3,38 +3,68 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 class User(db.Model):
+    __tablename__ = 'User'
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     username = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
 
-class Chore(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, ForeignKey('User.id'), nullable=False)
-    name = db.Column(db.String(120), unique=True, nullable=False)
-    duration = db.Column(db.Integer, unique=False, nullable=False)
-    date = db.Column(db.Datetime, unique=False, nullable=True)
-
-class Team(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column (db.Integer, ForeignKey('User.id'), nullable=False)
-    name = db.Column(db.String(120), unique=True, nullable=False)
-
-class Metrics(db.Model):
-    user_id = db.Column(db.Integer, ForeignKey('User.id'), nullable=False)
-    chore_id = db.Column(db.Integer, ForeignKey('Chore.id'), nullable=False)
-    chore_name = db.Column(db.String(120),ForeignKey('Chore.name'), nullable=False)
-    total_duration = db.Column(db.Integer, unique=False, nullable=False)
-    time_range = db.Column(db.Datetime, unique=False, nullable=True)
-
-    
-    def __repr__(self):
-        return f'<User {self.email}>'
-
     def serialize(self):
         return {
             "id": self.id,
             "email": self.email,
-            # do not serialize the password, its a security breach
+            "username": self.username,
         }
+
+class Chore(db.Model):
+    __tablename__ = 'Chore'
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('User.id'), nullable=False)
+    users = db.relationship(User)
+    name = db.Column(db.String(120), unique=True, nullable=False)
+    duration = db.Column(db.Integer, unique=False, nullable=False)
+    date = db.Column(db.Date, unique=False, nullable=True)
+
+    def serialize(chore):
+        return {
+            "id": chore.id,
+            "user_id": chore.user_id,
+            "name": chore.name,
+            "duration": chore.duration,
+            "date": chore.date,
+        }
+
+class Team(db.Model):
+    __tablename__ = 'Team'
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column (db.Integer, db.ForeignKey('User.id'), nullable=False)
+    users = db.relationship(User)
+    name = db.Column(db.String(120), unique=True, nullable=False)
+    
+    def serialize(team):
+        return{
+            "id": team.id,
+            "user_id": team.user_id,
+            "name": team.name,
+        }
+
+class Metrics(db.Model):
+    __tablename__ = 'Metrics'
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('User.id'), nullable=False)
+    chore_id = db.Column(db.Integer, db.ForeignKey('Chore.id'), nullable=False)
+    chore_name = db.Column(db.String(120),db.ForeignKey('Chore.name'), nullable=False)
+    total_duration = db.Column(db.Integer, unique=False, nullable=False)
+    time_range = db.Column(db.Date, unique=False, nullable=True)
+
+    def serialize(metrics):
+            return{
+                "user_id": metrics.user_id,
+                "chore_id": metrics.chore_id,
+                "chore_name": metrics.chore_name,
+                "total_duration": metrics.total_duration,
+                "time_range": metrics.time_range,
+            }
+        
+   
