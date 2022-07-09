dec84b4d9e3438567050e6e2c82d361461a1190e
christianmartinroffey <c.martinroffey@gmail.com>
2022-07-09 11:12:19 +0000

    db models sync fix

--- migrations/versions/25e69dbf2dac_.py
+++ migrations/versions/25e69dbf2dac_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: e7722f3a291e
+Revision ID: 25e69dbf2dac
 Revises: 
-Create Date: 2022-07-08 14:51:01.535319
+Create Date: 2022-07-09 10:57:42.711583
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = 'e7722f3a291e'
+revision = '25e69dbf2dac'
 down_revision = None
 branch_labels = None
 depends_on = None
---
+++ src/front/js/pages/Login.js
@@ -0,0 +1,31 @@
+import React, { useContext, useState } from "react";
+import { Context } from "../store/appContext";
+import "../../styles/home.css";
+
+function Signup() {
+  const { store, actions } = useContext(Context);
+  const [email, setEmail] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState ("");
+  // const token = localStorage.getItem("token");
+  // console.log("new user created", token)
+
+//   const handleClick = () => {
+//     actions.createNewUser(email, username, password);
+// };
+
+return (
+    <div className="text-center mt-5">
+      <h1 className="mb-3">Login</h1>
+    <div>
+      <input type="text" placeholder="email" value = {email} onChange={(event) => setEmail(event.target.value)}/> 
+      <input type="text" placeholder="username" value = {username} onChange={(event) => setUsername(event.target.value)}/>
+      <input type="password" placeholder="password" value = {password} onChange={(event) => setPassword(event.target.value)}/>
+      <button onClick={handleClick}> Submit </button>
+    </div>   
+    <h4 className="mt-3"> Don't yet have an account? <a href= "/signup"> Signup through here </a></h4>  
+  </div>
+)
+};
+
+export default Signup
