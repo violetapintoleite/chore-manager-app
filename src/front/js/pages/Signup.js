d6b39fabae976484ad3629ea17aaab249d999529
christianmartinroffey <c.martinroffey@gmail.com>
2022-07-12 17:42:02 +0000

    committing minor changes to the password length validation that is still in progress

--- src/front/js/pages/Signup.js
+++ src/front/js/pages/Signup.js
@@ -8,15 +8,32 @@ function Signup() {
   const [email, setEmail] = useState("");
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState ("");
+  const [btnDisabled, setbtnDisabled] = useState (true);
+  const [message, setMessage] = useState("");
+  
   const token = localStorage.getItem("token");
   
-    //redirect if token is present (i.e. they're logged in NEEDS FIGURING OUT)-
   const navigate = useNavigate();
   
   if(token && token != "" && token != undefined) navigate('/profile');
-  
+
+  const checkPassword = () => {
+    if(password === ""){
+      // setbtnDisabled(true)
+      setMessage("text")
+    } else if (password !== "" && password.length <= 8)
+    { 
+      // setbtnDisabled(true)
+      setMessage("password needs to be at least 8 characters")
+    }
+    else{
+      setMessage(null)
+      // setbtnDisabled(false)
+    };
+  };
 
   const handleClick = () => {
+    checkPassword();
     actions.createNewUser(email, username, password);
 };
 
@@ -27,11 +44,15 @@ return (
       <input type="text" placeholder="email" value = {email} onChange={(event) => setEmail(event.target.value)}/> 
       <input type="text" placeholder="username" value = {username} onChange={(event) => setUsername(event.target.value)}/>
       <input type="password" placeholder="password" value = {password} onChange={(event) => setPassword(event.target.value)}/>
-      <button onClick={handleClick}> Submit </button>
+      <button onClick={handleClick} isDisabled={btnDisabled}> Submit </button>
+      { message &&  <div className="message">{message}</div>}
     </div>   
     <h4 className="mt-3"> Already have an account? <a href= "/login"> Login through here </a></h4>  
+   
   </div>
+  
 )
+
 };
 
 export default Signup
