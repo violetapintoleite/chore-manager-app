7b18c9f7381116c46fa40429adfc4f27d880098b
christianmartinroffey <c.martinroffey@gmail.com>
2022-07-13 09:56:25 +0000

    set the database URL in .env file to save time with different instances

--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -34,7 +34,7 @@ const getState = ({ getStore, getActions, setStore }) => {
 
 				try {
 					const resp = await fetch(
-					  "https://3001-violetapint-choremanage-oo09xmgh41v.ws-eu53.gitpod.io/api/signup",
+						process.env.BACKEND_URL + "api/signup",
 					  opts
 					);
 					
@@ -82,7 +82,7 @@ const getState = ({ getStore, getActions, setStore }) => {
 		
 				try {
 				  const resp = await fetch(
-					"https://3001-violetapint-choremanage-ng4vm0smnco.ws-eu53.gitpod.io/api/login",
+					process.env.BACKEND_URL + "api/login",
 					opts
 			  		)
 			  
@@ -114,7 +114,7 @@ const getState = ({ getStore, getActions, setStore }) => {
 				try {
 				  // fetching data from the backend
 				  const resp = await fetch(
-					"https://3001-violetapint-choremanage-ng4vm0smnco.ws-eu53.gitpod.io/api/profile",
+					process.env.BACKEND_URL + "api/profile",
 					opts
 				  );
 				  
@@ -131,34 +131,6 @@ const getState = ({ getStore, getActions, setStore }) => {
 			// 	getActions().changeColor(0, "green");
 			// },
 
-			// getMessage: async () => {
-			// 	try{
-			// 		// fetching data from the backend
-			// 		const resp = await fetch(process.env.BACKEND_URL + "/api/hello")
-			// 		const data = await resp.json()
-			// 		setStore({ message: data.message })
-			// 		// don't forget to return something, that is how the async resolves
-			// 		return data;
-			// 	}catch(error){
-			// 		console.log("Error loading message from backend", error)
-			// 	}
-			// },
-			
-			changeColor: (index, color) => {
-				//get the store
-				const store = getStore();},
-
-      // getMessage: async () => {
-      // 	try{
-      // 		// fetching data from the backend
-      // 		const resp = await fetch(process.env.BACKEND_URL + "/api/hello")
-      // 		const data = await resp.json()
-      // 		setStore({ message: data.message })
-      // 		// don't forget to return something, that is how the async resolves
-      // 		return data;
-      // 	}catch(error){
-      // 		console.log("Error loading message from backend", error)
-      // 	}
       // },
       changeColor: (index, color) => {
         //get the store
@@ -213,7 +185,7 @@ const getState = ({ getStore, getActions, setStore }) => {
 
         try {
           const resp = await fetch(
-            "https://3001-violetapint-choremanage-oo09xmgh41v.ws-eu53.gitpod.io/api/chore",
+            process.env.BACKEND_URL + "api/chore",
             opts
           );
 
