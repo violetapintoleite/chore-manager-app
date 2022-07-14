cc02f61fec35d61351b70c105b5df4a8ad936db4
christianmartinroffey <c.martinroffey@gmail.com>
2022-07-14 18:31:19 +0000

    removing demo function causing error

--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -174,14 +174,7 @@ const getState = ({ getStore, getActions, setStore }) => {
 
         //we have to loop the entire demo array to look for the respective index
         //and change its color
-        const demo = store.demo.map((elm, i) => {
-          if (i === index) elm.background = color;
-          return elm;
-        });
-
-        //reset the global store
-        setStore({ demo: demo });
-      },
+      
 
       setChoreList: (chore, date, duration) => {
         const store = getStore();
