9c73be71c5d150953c236c6f2f51411480ba8121
Violeta Pinto Leite <violetalopesalves@gmail.com>
2022-07-19 08:56:20 +0000

    added path when logout

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -2,54 +2,64 @@ import React, { useContext, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { Context } from "../store/appContext";
 
-
 export const Navbar = () => {
   const { store, actions, token } = useContext(Context);
 
+  useEffect(() => {
+    if (store?.token && token != "") {
+      actions.checkIfAuthorized();
+    }
+  }, [store.token]);
 
-	useEffect(() => {
-        if(store?.token && token != "" ){
-        actions.checkIfAuthorized()
-        }
-	  },[ store.token] );
-
-	return (
-		<nav className="navbar navbar-light bg-light">
-			<div className="container">
-				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-				</Link>
-				<div className="">
-				{store.token ? (
-					<><Link to="/profile">
-						<button className="btn btn-primary m-2">Profile</button>
-					</Link><Link to="/history">
-						<button className="btn btn-primary m-2">My Chores</button>
-					</Link></> ) : (
-						<p></p>
-					) }
-				</div>
-				<div className="ml-auto">
-					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
-					</Link>
-					<Link to="/signup">
-						<button className="btn btn-primary m-2">Sign Up </button>
-					</Link>
-					<div className="">
-                  		{!store.token ? (
-                    <Link to="/login">
-                      <button className="btn btn-primary m-2">Login</button>
-                    </Link>
-                  ) : (
-                    <button className="btn btn-primary m-2" onClick={() => actions.logout()}>Logout</button>
-                  )}
-                </div>
-				</div>
-			</div>
-		</nav>
-	);
-
+  return (
+    <nav className="navbar navbar-light bg-light">
+      <div className="container">
+        <Link to="/">
+          <span className="navbar-brand mb-0 h1">React Boilerplate</span>
+        </Link>
+        <div className="">
+          {store.token ? (
+            <>
+              <Link to="/profile">
+                <button className="btn btn-primary m-2">Profile</button>
+              </Link>
+              <Link to="/history">
+                <button className="btn btn-primary m-2">My Chores</button>
+              </Link>
+            </>
+          ) : (
+            <p></p>
+          )}
+        </div>
+        <div className="ml-auto">
+          <Link to="/demo">
+            <button className="btn btn-primary">
+              Check the Context in action
+            </button>
+          </Link>
+          <Link to="/signup">
+            <button className="btn btn-primary m-2">Sign Up </button>
+          </Link>
+          <div className="">
+            {!store.token ? (
+              <Link to="/login">
+                <button className="btn btn-primary m-2">Login</button>
+              </Link>
+            ) : (
+              <Link to="/">
+                <button
+                  className="btn btn-primary m-2"
+                  onClick={() => actions.logout()}
+                >
+                  Logout
+                </button>
+              </Link>
+            )}
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
 
   return (
     <nav className="navbar navbar-light bg-light">
