0fe27612ef15a9b362abb4d957b68bc1492762ba
christianmartinroffey <c.martinroffey@gmail.com>
2022-07-20 19:05:37 +0000

    refactoring to bring in the YTiFrame code and complete the functionality

--- src/front/js/component/YTSearch.js
+++ src/front/js/component/YTSearch.js
@@ -5,7 +5,7 @@ const API_KEY= process.env.YT_API_KEY;
 
 const VideoSearch = () => {
   
-  const [video, setVideo] = useState("");
+  const [videoID, setVideoID] = useState("");
 
   useEffect(() => {
 
@@ -14,24 +14,53 @@ const VideoSearch = () => {
       const firstVideoResult = videos[1];
       const videoValues = Object.values(firstVideoResult);
 
-      setVideo(firstVideoResult);
+      setVideoID(firstVideoResult);
       console.log(firstVideoResult);
       console.log(videoValues);
     });
   };
 
+  console.log(videoID);
+
   videoSearch("little l");
 
 }, []);
    
+
+const [videoUrl, setVideoUrl] = useState("");
+const embedURL = "https://www.youtube.com/embed/";
+let videoCode;
+
+    if (videoUrl) {
+    videoCode = embedURL.concat(videoUrl.split("v=")[1].split("&")[0]);
+    };
+
+    //note to tweak the above - need to concat the video ID only, something like
+    // videoCode = embedURL.concat(video.id)
+  
+
+
+const searchHandler = (term) =>{
+ // need to pass the 'term' through the onclick submit in here and set that as the setVideoID state
+ //then take that and append/concat that to embedURL variable above
+  setVideoID(term);
+
+};
+
   return (
     <div>
       <p>VideoSearch</p>
-      <p>{video}</p>
-      {/* <div class="input-group mb-3">
-    <input type="text" class="form-control" placeholder="Recipient's username" aria-label="Recipient's username" aria-describedby="button-addon2">
-    <button class="btn btn-outline-secondary" type="button" id="button-addon2">Button</button>
-  </div> */}
+      <p>{videoID}</p>
+      <div class="input-group mb-3">
+      <input type="text" class="form-control" placeholder="Search in Youtube" aria-label="Recipient's username" aria-describedby="button-addon2" onChange={(event) => videoSearch(event.target.value)}></input>
+      <button class="btn btn-outline-secondary" value = {videoCode} onChange={(event) => setVideoUrl(event.target.value)} type="button" id="button-addon2">Search</button>
+      </div>
+      <input type="text" placeholder="Youtube URL" className="form-control mb-2" value = {videoCode} onChange={(event) => setVideoUrl(event.target.value)}/>
+                {console.log(videoUrl)}
+                    { !videoUrl || videoUrl== "" ? <p>Add your video above</p> : ""}
+                    <div class="ratio ratio-16x9">
+                      <iframe src={videoCode} allow="autoplay;" value="player" allowfullscreen></iframe>
+                    </div>
     </div>
   )
   
