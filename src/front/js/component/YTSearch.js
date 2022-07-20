afb22cab24bea3350e01f913b3309c62ff5dd6c8
christianmartinroffey <c.martinroffey@gmail.com>
2022-07-20 18:42:44 +0000

    getting quota limit so can't finish rendering the data from the object

--- src/front/js/component/YTSearch.js
+++ src/front/js/component/YTSearch.js
@@ -1,20 +1,22 @@
 import React, { useState, useEffect } from 'react';
 import  YTSearch  from "youtube-api-search";
 
-const API_KEY= process.env.YT_API_KEY
+const API_KEY= process.env.YT_API_KEY;
 
 const VideoSearch = () => {
   
-  const [videos, setVideos] = useState("");
+  const [video, setVideo] = useState("");
 
   useEffect(() => {
 
   const videoSearch = (term) => {
     YTSearch({ key: API_KEY, term: term }, videos => {
-      const videoResults = videos[0].id;
+      const firstVideoResult = videos[1];
+      const videoValues = Object.values(firstVideoResult);
 
-      // setVideos(videoResults);
-      console.log(videoResults);
+      setVideo(firstVideoResult);
+      console.log(firstVideoResult);
+      console.log(videoValues);
     });
   };
 
@@ -25,7 +27,7 @@ const VideoSearch = () => {
   return (
     <div>
       <p>VideoSearch</p>
-      <p>{videos}</p>
+      <p>{video}</p>
       {/* <div class="input-group mb-3">
     <input type="text" class="form-control" placeholder="Recipient's username" aria-label="Recipient's username" aria-describedby="button-addon2">
     <button class="btn btn-outline-secondary" type="button" id="button-addon2">Button</button>
