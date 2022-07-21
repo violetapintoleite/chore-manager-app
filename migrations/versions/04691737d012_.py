b25dcdf78b9c16f571dbce4a79722728639a7580
christianmartinroffey <c.martinroffey@gmail.com>
2022-07-21 18:55:35 +0000

    YT search functionality working

--- migrations/versions/04691737d012_.py
+++ migrations/versions/04691737d012_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: ac5ae2c08092
+Revision ID: 04691737d012
 Revises: 
-Create Date: 2022-07-19 13:41:21.886636
+Create Date: 2022-07-21 16:49:18.458989
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = 'ac5ae2c08092'
+revision = '04691737d012'
 down_revision = None
 branch_labels = None
 depends_on = None
--- src/front/js/component/YTSearch.js
+++ src/front/js/component/YTSearch.js
@@ -1,69 +1,61 @@
-import React, { useState, useEffect } from 'react';
-import  YTSearch  from "youtube-api-search";
+import React, { useState, useEffect } from "react";
+import YTSearch from "youtube-api-search";
 
-const API_KEY= process.env.YT_API_KEY;
+const API_KEY = process.env.YT_API_KEY;
 
 const VideoSearch = () => {
-  
   const [videoID, setVideoID] = useState("");
+  const [searchTerm, setSearchTerm] = useState("");
+  const embedURL = "https://www.youtube.com/embed/";
+  let videoCode = embedURL.concat(videoID);
 
-  useEffect(() => {
-
-  const videoSearch = (term) => {
-    YTSearch({ key: API_KEY, term: term }, videos => {
-      const firstVideoResult = videos[1];
-      const videoValues = Object.values(firstVideoResult);
+  const searchYouTube = (term) => {
+    console.log(term, searchTerm);
+    YTSearch({ key: API_KEY, term: term }, (videos) => {
+      const firstVideoResult = videos[0].id.videoId;
 
       setVideoID(firstVideoResult);
-      console.log(firstVideoResult);
-      console.log(videoValues);
+      console.log(firstVideoResult, "returning obj of the first result");
     });
   };
 
-  console.log(videoID);
-
-  videoSearch("little l");
-
-}, []);
-   
-
-const [videoUrl, setVideoUrl] = useState("");
-const embedURL = "https://www.youtube.com/embed/";
-let videoCode;
-
-    if (videoUrl) {
-    videoCode = embedURL.concat(videoUrl.split("v=")[1].split("&")[0]);
-    };
-
-    //note to tweak the above - need to concat the video ID only, something like
-    // videoCode = embedURL.concat(video.id)
-  
-
-
-const searchHandler = (term) =>{
- // need to pass the 'term' through the onclick submit in here and set that as the setVideoID state
- //then take that and append/concat that to embedURL variable above
-  setVideoID(term);
-
-};
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    searchYouTube(searchTerm);
+  };
 
   return (
     <div>
       <p>VideoSearch</p>
       <p>{videoID}</p>
       <div class="input-group mb-3">
-      <input type="text" class="form-control" placeholder="Search in Youtube" aria-label="Recipient's username" aria-describedby="button-addon2" onChange={(event) => videoSearch(event.target.value)}></input>
-      <button class="btn btn-outline-secondary" value = {videoCode} onChange={(event) => setVideoUrl(event.target.value)} type="button" id="button-addon2">Search</button>
+        <form className="d-flex">
+          <input
+            type="text"
+            class="form-control"
+            placeholder="Search in Youtube"
+            aria-label="Recipient's username"
+            aria-describedby="button-addon2"
+            value={searchTerm}
+            onChange={(event) => setSearchTerm(event.target.value)}
+          ></input>
+          <button
+            class="btn btn-outline-secondary"
+            onSubmit={handleSubmit}
+            onClick={handleSubmit}
+            type="button"
+            id="button-addon2"
+          >
+            Search
+          </button>
+        </form>
       </div>
-      <input type="text" placeholder="Youtube URL" className="form-control mb-2" value = {videoCode} onChange={(event) => setVideoUrl(event.target.value)}/>
-                {console.log(videoUrl)}
-                    { !videoUrl || videoUrl== "" ? <p>Add your video above</p> : ""}
-                    <div class="ratio ratio-16x9">
-                      <iframe src={videoCode} allow="autoplay;" value="player" allowfullscreen></iframe>
-                    </div>
+      {console.log(videoID)}
+      <div class="ratio ratio-16x9">
+        <iframe src={videoCode} allow="autoplay;" value="player"></iframe>
+      </div>
     </div>
-  )
-  
+  );
 };
 
-export default VideoSearch
+export default VideoSearch;
