93502c122081dd8be689fa9ba0911f4cb821f31f
Violeta Pinto Leite <violetalopesalves@gmail.com>
2022-07-26 17:38:23 +0000

    fixed small bug

--- src/front/js/pages/profile.js
+++ src/front/js/pages/profile.js
@@ -1,36 +1,32 @@
-import React, { useContext, useEffect } from 'react'
+import React, { useContext, useEffect } from "react";
 import { Context } from "../store/appContext";
-import { useNavigate } from 'react-router-dom';
-import YTiframe from '../component/YTiframe';
-import AddToTeam from '../component/AddToTeam';
-import VideoSearch from '../component/YTSearch';
-
-
-
+import { useNavigate } from "react-router-dom";
+import YTiframe from "../component/YTiframe";
+import AddToTeam from "../component/AddToTeam";
+import VideoSearch from "../component/YTSearch";
 
 function Profile() {
-    const { store, actions } = useContext(Context);
-    const token = localStorage.getItem("token");
-    const navigate = useNavigate();
-    console.log( store.team)
+  const { store, actions } = useContext(Context);
+  const token = localStorage.getItem("token");
+  const navigate = useNavigate();
+  console.log(store.team);
 
-    //redirect to the /login page if there is no token present
-  if(!token || token == "" || token == undefined) navigate('/login')
+  //redirect to the /login page if there is no token present
+  if (!token || token == "" || token == undefined) navigate("/login");
 
   return (
     <div>
-
-        <h5>Logged in as {store.email}</h5>
-        <AddToTeam></AddToTeam>
-        <div className='card'>
-          <YTiframe></YTiframe>
+      <h5>Logged in as {store.email}</h5>
+      <AddToTeam></AddToTeam>
+      <div className="card">
+        <YTiframe></YTiframe>
 
         <h1>{store.email}</h1>
-        
-        <VideoSearch></VideoSearch>
 
+        <VideoSearch></VideoSearch>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Profile
+export default Profile;
