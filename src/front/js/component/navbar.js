4c6644728a2f4307bdd8509e8bfc5a7df0470f33
christianmartinroffey <c.martinroffey@gmail.com>
2022-07-27 12:13:13 +0000

    styling changes to the navbar

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -14,49 +14,87 @@ export const Navbar = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <div className="container">
-       
-          <Link to="/">
-            <span className="navbar-brand mb-0 h1">Chore Manager</span>
-          </Link>
-          <div className="">
-            {store.token ? (
+        <Link to="/">
+          <span className="navbar-brand mb-0 h1">Chore Manager</span>
+        </Link>
+        {store.token ? (
+        <p class="nav-item dropdown">
+          {" "}
+          Menu
+          <a
+            class="nav-link dropdown-toggle"
+            id="navbarDropdownMenuLink"
+            role="button"
+            data-bs-toggle="dropdown"
+            aria-expanded="false"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M4 6h16M4 10h16M4 14h16M4 18h16"
+            />
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+            
               <>
-                <Link to="/profile">
-                  <button className="btn btn-primary m-2 nav-link">Dashboard</button>
-                </Link>
-                <Link to="/history">
-                  <button className="btn btn-primary m-2 nav-link">My Chores</button>
-                </Link>
-                <Link to="/metrics">
-                  <button className="btn btn-primary m-2 nav-link">Metrics</button>
-                </Link>
+                <li>
+                  {" "}
+                  <Link to="/profile">
+                    <a className="dropdown-item" href="/profile">
+                      Dashboard
+                    </a>{" "}
+                  </Link>
+                </li>
+                <li>
+                  {" "}
+                  <Link to="/history">
+                    <a class="dropdown-item" href="/history">
+                      My Chores
+                    </a>{" "}
+                  </Link>
+                </li>
+                <li>
+                  {" "}
+                  <Link to="/metrics">
+                    <a class="dropdown-item" href="#">
+                      My Metrics
+                    </a>
+                  </Link>
+                </li>
+                <li>
+                  {" "}
+                  <Link to="/">
+                    <button
+                      className="btn btn-primary m-2 nav-link"
+                      onClick={() => actions.logout()}
+                    >
+                      Logout
+                    </button>
+                  </Link>
+                </li>
               </>
-            ) : (
-              <p></p>
-            )}
-          </div>
-
+          
+            
+          </ul>
+        </p>
+        ) : (
+          <p></p> )}
+        {!store.token ? (
+          <Link to="/login">
+            <button className="btn btn-primary m-2 nav-link">Login </button>
+          </Link>
+        ) : (
+          <p></p>
+        )}
+        {!store.token ? (
           <Link to="/signup">
             <button className="btn btn-primary m-2 nav-link">Sign Up</button>
           </Link>
-          <div className="">
-            {!store.token ? (
-              <Link to="/login">
-                <button className="btn btn-primary m-2 nav-link">Login</button>
-              </Link>
-            ) : (
-              <Link to="/">
-                <button
-                  className="btn btn-primary m-2 nav-link"
-                  onClick={() => actions.logout()}
-                >
-                  Logout
-                </button>
-              </Link>
-            )}
-          </div>
-        </div>
-     
+        ) : (
+          <p></p>
+        )}
+        <div className=""></div>
+      </div>
     </nav>
   );
 
--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -8,9 +8,8 @@ export const Home = () => {
 
 	return (
 		<div className="text-center mt-5">
-			
 			<InspoQuote></InspoQuote>
-			
+
 		</div>
 	);
 };
