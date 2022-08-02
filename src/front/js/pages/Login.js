907174af7e8ba3e50e59502a7a11365b6aa15385
Violeta Pinto Leite <violetalopesalves@gmail.com>
2022-08-02 13:53:46 +0000

    centered login and signup forms and disabled submit button on login page like it was for signup

--- src/front/js/pages/Login.js
+++ src/front/js/pages/Login.js
@@ -1,38 +1,64 @@
 import React, { useContext, useState } from "react";
 import { Context } from "../store/appContext";
 import "../../styles/home.css";
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
+import "../../styles/modules/hometext.css";
 
 function Login() {
   const { store, actions } = useContext(Context);
   const [email, setEmail] = useState("");
   const [username, setUsername] = useState("");
-  const [password, setPassword] = useState ("");
+  const [password, setPassword] = useState("");
   // use this to then push to specific page if token is present
   const token = localStorage.getItem("token");
   const navigate = useNavigate();
 
   //redirects to protected page is token is present in localstorage
-  if(token && token != "" && token != undefined ) navigate('/profile');
-  
+  if (token && token != "" && token != undefined) navigate("/profile");
+
   const handleClick = () => {
     actions.login(email, username, password);
-    
-};
+  };
 
+  return (
+    <div className="home position-relative">
+      <div className="position-absolute top-50 start-50 translate-middle">
+        <h1 className="mb-3 text-center">Login</h1>
+        <div>
+          <input
+            type="text"
+            placeholder="email"
+            value={email}
+            onChange={(event) => setEmail(event.target.value)}
+          />
+          <input
+            type="text"
+            placeholder="username"
+            value={username}
+            onChange={(event) => setUsername(event.target.value)}
+          />
+          <input
+            type="password"
+            placeholder="password"
+            value={password}
+            onChange={(event) => setPassword(event.target.value)}
+          />
+          <button
+            className="btn"
+            onClick={handleClick}
+            disabled={password.length < 8 || username.length < 2}
+          >
+            {" "}
+            Submit{" "}
+          </button>
+        </div>
+        <h4 className="mt-3">
+          {" "}
+          Don't yet have an account? <a href="/signup"> Signup through here </a>
+        </h4>{" "}
+      </div>
+    </div>
+  );
+}
 
-return (
-    <div className="text-center mt-5">
-      <h1 className="mb-3">Login</h1>
-    <div>
-      <input type="text" placeholder="email" value = {email} onChange={(event) => setEmail(event.target.value)}/> 
-      <input type="text" placeholder="username" value = {username} onChange={(event) => setUsername(event.target.value)}/>
-      <input type="password" placeholder="password" value = {password} onChange={(event) => setPassword(event.target.value)}/>
-      <button className="btn" onClick={handleClick}> Submit </button>
-    </div>   
-    <h4 className="mt-3"> Don't yet have an account? <a href= "/signup"> Signup through here </a></h4>  
-  </div>
-)
-};
-
-export default Login
+export default Login;
