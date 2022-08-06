7ca125412149d3581cd13ccd8dc92034ba723e1e
christianmartinroffey <c.martinroffey@gmail.com>
2022-08-06 09:31:35 +0000

    spacing and color changes to the walkthrough blocks on home

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -35,7 +35,7 @@ export const Home = () => {
           </div>
         </div>
 
-        <div className="text-center bg-dark animated-div">
+        <div className="text-center animated-div">
           <div className="pb-3 bg-white">
             <div className="animation-div d-flex justify-content-center">
               <div className="perspective-text mb-5">
@@ -68,26 +68,26 @@ export const Home = () => {
             </div> */}
           <div className=" first-section">
             <div className="d-flex m-2">
-              <div className="card  radius-3px col-5">
+              <div className="card radius-3px col-5">
                 <img
                   src={laptopimg}
                   className="home-img"
                   alt="home-img-light"
                 />
               </div>
-              <div className="container  d-flex first-section radius-3px align-middle">
+              <div className="container d-flex text-section first-section radius-3px align-middle">
                 <div className="container first-section col-3 pt-4 ">
-                  <h4 className="homepage-numbers">1</h4>
+                  <h4 className="homepage-numbers text-white">1</h4>
                 </div>
                 <div className="container first-section col-4 pt-4">
-                  <h3 className="pt-2">Create an account</h3>
-                  <p>
+                  <h3 className="pt-2 text-white">Create an account</h3>
+                  <p className="text-white">
                     Go to our Sign Up page and create an account there using
                     your email. You'll be able to create a username and password
                     for your account.
                   </p>
                   <Link to="/signup">
-                    <button className="btn btn-text align-center">
+                    <button className="btn btn-white-styling align-center">
                       {" "}
                       Sign Up Now!
                     </button>
@@ -96,20 +96,22 @@ export const Home = () => {
               </div>
             </div>
           </div>
-          <div className="bg-dark p-3">
-            <div className="d-flex m-2 pt-4">
-              <div className="container bg-dark col-3 pt-4">
-                <h4 className="homepage-numbers text-white">2</h4>
+          <hr />
+          <div className=" p-3">
+            <div className="d-flex m-2 pt-4 ">
+              <div className="text-section second-section d-flex">
+                <div className="container second-section col-3 pt-4">
+                  <h4 className="homepage-numbers text-white">2</h4>
+                </div>
+                <div className="container second-section  text-white col-4 pt-4">
+                  <h3 className="pt-2">Log your chore in 'My Chores'</h3>
+                  <p className="pb-4">
+                    In our custom built Chore tracking page you'll be able to
+                    select the chore you about to complete. Once you're done,
+                    you can add in the time it took.
+                  </p>
+                </div>
               </div>
-              <div className="container bg-dark text-white col-3 pt-4">
-                <h3 className="pt-2">Log your chore in 'My Chores'</h3>
-                <p>
-                  In our custom built Chore tracking page you'll be able to
-                  select the chore you about to complete. Once you're done, you
-                  can add in the time it took.
-                </p>
-              </div>
-              <div className="col-1"></div>
               <div className="card radius-3px col-5">
                 <img
                   src={laptopimglight}
@@ -119,38 +121,42 @@ export const Home = () => {
               </div>
             </div>
           </div>
-
+          <hr />
           <div className="d-flex m-2">
             <div className="card radius-3px col-5">
               <img src={laptopimg} className="home-img" alt="home-img-light" />
             </div>
-            <div className="container third-section d-flex pt-4">
+            <div className="container third-section text-section d-flex pt-4">
               <div className="container third-section col-3 ">
-                <h4 className="homepage-numbers">3</h4>
+                <h4 className="homepage-numbers text-white">3</h4>
               </div>
               <div className="container third-section col-4 pt-4">
-                <h3 className="pt-2">Take a look at your metrics</h3>
-                <p>
+                <h3 className="pt-2 text-white">Take a look at your metrics</h3>
+                <p className="text-white">
                   Every chore you add will get added to your personal metrics
                   overview in your Dashboard.
                 </p>
               </div>
             </div>
           </div>
-          <div className="bg-dark p-3">
-            <div className="d-flex m-2 pt-4">
-              <div className="container bg-dark col-3 pt-4">
+          <hr />
+          <div className=" p-3">
+            <div className="d-flex m-2 pt-4 ">
+              <div className="text-section fourth-section d-flex">
+                <div className="container fourth-section col-3 pt-4">
                 <h4 className="homepage-numbers text-white">4</h4>
               </div>
-              <div className="container text-white col-3 ">
+              <div className="container text-white col-4 pt-4">
                 <h3 className="pt-2">Add yourself to a team</h3>
-                <p>
+                <p className="pb-4">
                   See how you compare to other people on your team. Go to the
                   Team Metrics overview to add yourself to a team and see the
                   team's results.
                 </p>
+
               </div>
-              <div className="col-1"></div>
+              </div>
+              
               <div className="card radius-3px col-5">
                 <img
                   src={laptopimglight}
@@ -165,22 +171,3 @@ export const Home = () => {
     </>
   );
 };
-
-{
-  /* <div className="">
-        <h2>
-          With Chore Manager you can track your chores to find out how much time
-          you spend completing your household tasks
-        </h2>
-        <h3> What you will find in the app</h3>
-        <ul>
-          <li>A list of the most common household chores</li>
-          <li>A way to track how long it took you to complete the chore</li>
-        </ul>
-        <h3> Sign up here </h3>
-        <h3> Already got an account? Log in through here instead</h3>
-        <h3>
-          Want to split your chores with others? Why not join a team with others
-        </h3>
-        </div> */
-}
