38fdc70c943bc176184a8ad4a01c9a0fb506047a
christianmartinroffey <c.martinroffey@gmail.com>
2022-08-09 10:58:19 +0000

    padding changes to cards on homepage

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -31,114 +31,116 @@ export const Home = () => {
             <InspoQuote></InspoQuote>
           </div>
         </div>
-      {/*cards section */}
-          <div className="">
-            <div className="d-flex m-2 homepage-card-container ">
-              <div className="card bg-white shadow-none radius-3px col-5 ">
-                <img
-                  src={laptopimg}
-                  className="home-img home-img-appear"
-                  alt="home-img-light"
-                />
-              </div>
-              <div className="container d-flex text-section first-section align-middle pb-4">
-                <div className="container first-section  col-3 pt-4 ">
-                  <h4 className="homepage-numbers text-white">1</h4>
-                </div>
-                <div className="container first-section col-4 pt-4">
-                  <h3 className="pt-2 text-white">Create an account</h3>
-                  <p className="text-white">
-                    Go to our Sign Up page and create an account there using
-                    your email. You'll be able to create a username and password
-                    for your account.
-                  </p>
-                  <Link to="/signup">
-                    <button className="btn btn-white-styling align-center">
-                      {" "}
-                      Sign Up Now!
-                    </button>
-                  </Link>
-                </div>
-              </div>
+        {/*cards section */}
+        <div className="">
+          <div className="d-flex m-2 homepage-card-container ">
+            <div className="card bg-white shadow-none radius-3px col-5 ">
+              <img
+                src={laptopimg}
+                className="home-img home-img-appear"
+                alt="home-img-light"
+              />
             </div>
-          </div>
-          <hr />
-          <div className=" p-3">
-            <div className="d-flex m-2 pt-4 homepage-card-container ">
-              <div className="text-section second-section d-flex">
-                <div className="container second-section col-3 pt-4">
-                  <h4 className="homepage-numbers text-white">2</h4>
-                </div>
-                <div className="container second-section  text-white col-4 pt-4">
-                  <h3 className="pt-2">Log your chore in 'My Chores'</h3>
-                  <p className="pb-4">
-                    In our custom built Chore tracking page you'll be able to
-                    select the chore you about to complete. Once you're done,
-                    you can add in the time it took.
-                  </p>
-                </div>
+            <div className="container d-flex text-section first-section align-middle pb-4">
+              <div className="container first-section  col-3 pt-4 ">
+                <h4 className="homepage-numbers text-white">1</h4>
               </div>
-              <div className="card radius-3px shadow-none col-5">
-                <img
-                  src={laptopimglight}
-                  className="home-img home-img-appear  "
-                  alt="home-img-light"
-                />
-              </div>
-            </div>
-          </div>
-          <hr />
-          <div className="d-flex homepage-card-container m-2">
-            <div className="card radius-3px shadow-none col-5">
-              <img src={laptopimg} className="home-img home-img-appear" alt="home-img-light" />
-            </div>
-            <div className="container third-section text-section d-flex pt-4">
-              <div className="container third-section col-3 ">
-                <h4 className="homepage-numbers text-white">3</h4>
-              </div>
-              <div className="container third-section col-4 pt-4">
-                <h3 className="pt-2 text-white">Take a look at your metrics</h3>
+              <div className="container first-section col-4 pt-4">
+                <h3 className="pt-2 text-white">Create an account</h3>
                 <p className="text-white">
-                  Every chore you add will get added to your personal metrics
-                  overview in your Dashboard.
+                  Go to our Sign Up page and create an account there using your
+                  email. You'll be able to create a username and password for
+                  your account.
+                </p>
+                <Link to="/signup">
+                  <button className="btn btn-white-styling align-center">
+                    {" "}
+                    Sign Up Now!
+                  </button>
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="pb-2">
+          <div className="d-flex m-2 pt-3 homepage-card-container ">
+            <div className="text-section second-section d-flex">
+              <div className="container second-section col-3 pt-4">
+                <h4 className="homepage-numbers text-white">2</h4>
+              </div>
+              <div className="container second-section  text-white col-4 pt-4">
+                <h3 className="pt-2">Log your chore in 'My Chores'</h3>
+                <p className="pb-4">
+                  In our custom built Chore tracking page you'll be able to
+                  select the chore you about to complete. Once you're done, you
+                  can add in the time it took.
                 </p>
               </div>
             </div>
-          </div>
-          <hr />
-          <div className=" p-3">
-            <div className="d-flex homepage-card-container m-2 pt-4 ">
-              <div className="text-section fourth-section d-flex">
-                <div className="container fourth-section col-3 pt-4">
-                  <h4 className="homepage-numbers text-white">4</h4>
-                </div>
-                <div className="container text-white col-4 pt-4">
-                  <h3 className="pt-2">Add yourself to a team</h3>
-                  <p className="pb-4">
-                    See how you compare to other people on your team. Go to the
-                    Team Metrics overview to add yourself to a team and see the
-                    team's results.
-                  </p>
-                </div>
-              </div>
-              <div className="card radius-3px shadow-none col-5">
-                <img
-                  src={laptopimglight}
-                  className="home-img home-img-appear"
-                  alt="home-img-light"
-                />
-              </div>
+            <div className="card radius-3px shadow-none col-5">
+              <img
+                src={laptopimglight}
+                className="home-img home-img-appear  "
+                alt="home-img-light"
+              />
             </div>
           </div>
-          <hr />
-          <br/>
-          <Play></Play>
-      
-          <HomepageTeamSection></HomepageTeamSection>
-          <hr />
-       
-          
         </div>
+
+        <div className="d-flex homepage-card-container m-2">
+          <div className="card radius-3px shadow-none col-5">
+            <img
+              src={laptopimg}
+              className="home-img home-img-appear"
+              alt="home-img-light"
+            />
+          </div>
+          <div className="container third-section text-section d-flex pt-4">
+            <div className="container third-section col-3 ">
+              <h4 className="homepage-numbers text-white">3</h4>
+            </div>
+            <div className="container third-section col-4 pt-4">
+              <h3 className="pt-2 text-white">Take a look at your metrics</h3>
+              <p className="text-white">
+                Every chore you add will get added to your personal metrics
+                overview in your Dashboard.
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div className="">
+          <div className="d-flex homepage-card-container m-2 pt-4 ">
+            <div className="text-section fourth-section d-flex">
+              <div className="container fourth-section col-3 pt-4">
+                <h4 className="homepage-numbers text-white">4</h4>
+              </div>
+              <div className="container text-white col-4 pt-4">
+                <h3 className="pt-2">Add yourself to a team</h3>
+                <p className="pb-4">
+                  See how you compare to other people on your team. Go to the
+                  Team Metrics overview to add yourself to a team and see the
+                  team's results.
+                </p>
+              </div>
+            </div>
+            <div className="card radius-3px shadow-none col-5">
+              <img
+                src={laptopimglight}
+                className="home-img home-img-appear"
+                alt="home-img-light"
+              />
+            </div>
+          </div>
+        </div>
+        <hr />
+        <br />
+        <Play></Play>
+
+        <HomepageTeamSection></HomepageTeamSection>
+        <hr />
+      </div>
     </>
   );
 };
