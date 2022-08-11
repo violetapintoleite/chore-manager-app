8f2697e4be9921c037ff1edbd127bc47d0c384fd
Violeta Pinto Leite <violetalopesalves@gmail.com>
2022-08-11 18:47:34 +0000

    styled signup form

--- src/front/js/pages/Signup.js
+++ src/front/js/pages/Signup.js
@@ -22,45 +22,87 @@ function Signup() {
     actions.createNewUser(email, username, password);
   };
 
-
-//email verification
-const [emailError, setEmailError] = useState('Please enter a valid email!')
+  //email verification
+  const [emailError, setEmailError] = useState("Please enter a valid email!");
 
   const validateEmail = (e) => {
     var email = e.target.value;
     if (validator.isEmail(email)) {
-
-      setEmailError('Email is valid!')
+      setEmailError("Email is valid!");
     } else {
-      setEmailError('Must be a valid email!')
+      setEmailError("Must be a valid email!");
     }
   };
 
-return (
-    <div className="text-center mt-5">
-      <h1 className="mb-3">Sign Up</h1>
-      <div className= "container">
-      <h3>Sign up requirements</h3>
-      <ul>
-        <span className="" >{emailError}</span><br/>
-        <span> {username == "" || username.length < 2? <span>Username must be at least 2 characters long</span> : <span></span>} </span> <br/>
-        <span> {password == "" || password.length < 8 ? <span className="p-2">Password needs to be at least 8 characters long</span> :  <span></span>} </span>
-      </ul>
+  return (
+    <div className="loginandsignuppageheight position-relative">
+      <div className="position-absolute top-50 start-50 translate-middle">
+        <h1 className="mb-3 text-center">Sign Up</h1>
+
+        <div>
+          <label className="form-label">Email address</label>
+          <input
+            type="email"
+            className="form-control"
+            placeholder="email"
+            value={email}
+            onChange={(event) => setEmail(event.target.value)}
+            onInput={(e) => validateEmail(e)}
+          />
+          <div className="text-danger">
+            <span className="">{emailError}</span>
+          </div>
+          <label className="form-label">Username</label>
+          <input
+            type="text"
+            className="form-control"
+            placeholder="username"
+            value={username}
+            onChange={(event) => setUsername(event.target.value)}
+          />
+          <div className="text-danger">
+            {username == "" || username.length < 2 ? (
+              <span>Username must be at least 2 characters long</span>
+            ) : (
+              <span></span>
+            )}
+          </div>
+          <label className="form-label">Password</label>
+          <input
+            type="password"
+            className="form-control"
+            placeholder="password"
+            value={password}
+            onChange={(event) => setPassword(event.target.value)}
+          />
+          <div className="text-danger">
+            {password == "" || password.length < 8 ? (
+              <span className="p-2">
+                Password needs to be at least 8 characters long
+              </span>
+            ) : (
+              <span></span>
+            )}
+          </div>
+        </div>
+        <br />
+        <div className="text-center">
+          <button
+            className="btn m-1 "
+            onClick={handleClick}
+            disabled={password.length < 8 || username.length < 2}
+          >
+            {" "}
+            Submit{" "}
+          </button>{" "}
+        </div>
+        <h4 className="mt-3">
+          {" "}
+          Already have an account? <a href="/login"> Login through here </a>
+        </h4>
       </div>
-    <div>
-      <input type="email" className="m-1 pb-1" placeholder="email" value={email} onChange={(event) => setEmail(event.target.value)} onInput= {(e) => validateEmail(e)} /> 
-      <input type="text" className="m-1 pb-1" placeholder="username" value={username} onChange={(event) => setUsername(event.target.value)}/>
-      <input type="password" className="m-1 pb-1" placeholder="password" value={password} onChange={(event) => setPassword(event.target.value)}/>
-      <button className="btn m-1 " onClick={handleClick} disabled={password.length < 8 || username.length < 2}> Submit </button> <br/>
-    </div>   
-    <h4 className="mt-3"> Already have an account? <a href= "/login"> Login through here </a></h4>     
-  </div>
-  
-)
+    </div>
+  );
+}
 
-};
-
-export default Signup
-
-
-
+export default Signup;
